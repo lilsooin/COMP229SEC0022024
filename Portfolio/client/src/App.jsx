65dde82cf97095497455b393d877950b205daaae
sooin <lilesooin@gmail.com>
2024-10-29 16:59:56 -0400

    migrate backend

--- Portfolio/client/src/App.jsx
+++ Portfolio/client/src/App.jsx
@@ -5,20 +5,14 @@ import MainRouter from '../MainRouter';
 //import Contact from './contact'
 //import theme from '../theme';
 //import { hot } from 'react-hot-loader'
-
-/**
- * Filename: App.jsx
- * Student’s Name: Suin Lee 
- * StudentID: 301369995
- * Date: 2024-09-24
- */
 const App = () => {
-  return (
-    <Router>
+ return (
+ <Router>
+ 
+ <MainRouter />
 
-      <MainRouter />
-
-    </Router>
-  );
+ </Router>
+ );
 };
 export default App;
+
